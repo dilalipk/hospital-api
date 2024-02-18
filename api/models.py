21c63896208a67065ef83e12f255657a57db569e
Dilshad Ali <dilalipk@gmail.com>
2024-02-18 10:33:14 +0500

    add readme.md  file and requirements.txt file

--- api/models.py
+++ api/models.py
@@ -38,6 +38,6 @@ class Appointment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
+    
     def __str__(self):
         return f"{self.date} - Dr. {self.doctor.name} - {self.patient.name}"
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Doctor Paitent APi
+This API, built using Django REST Framework, facilitates the management of doctor-patient appointments. It allows for the creation, retrieval, updating, and deletion of appointments between doctors and patients.
